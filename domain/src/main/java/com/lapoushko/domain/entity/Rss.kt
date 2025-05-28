a851ca7aede5e8adfe8452084fb7acbb49d59356
Lapoushko <vadim.dovgiy@mail.ru>
2025-05-29 01:54:06 +0500

    add domain

---
+++ domain/src/main/java/com/lapoushko/domain/entity/Rss.kt
@@ -0,0 +1,13 @@
+package com.lapoushko.domain.entity
+
+/**
+ * @author Lapoushko
+ */
+class Rss(
+    val title: String,
+    val link: String,
+    val description: String,
+    val pubDate: String,
+    val image: String,
+    val creator: String
+)
