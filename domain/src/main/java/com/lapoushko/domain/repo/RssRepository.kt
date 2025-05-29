febaf903f30c0ae09f72ede5664dd355b3b5ff57
Lapoushko <vadim.dovgiy@mail.ru>
2025-05-29 16:38:06 +0500

    fix

--- domain/src/main/java/com/lapoushko/domain/repo/RssRepository.kt
+++ domain/src/main/java/com/lapoushko/domain/repo/RssRepository.kt
@@ -1,25 +1,12 @@
 package com.lapoushko.domain.repo
 
-import com.lapoushko.domain.entity.Rss
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import javax.inject.Inject
+import com.lapoushko.domain.entity.Channel
 
 /**
  * @author Lapoushko
  */
 interface RssRepository {
-    fun getRss(): Flow<Rss>
+    suspend fun getChannel(): Channel
 
-    fun getCategories(): Flow<String>
-}
-
-class RssRepositoryImpl @Inject constructor(): RssRepository{
-    override fun getRss(): Flow<Rss> {
-        return flow {  }
-    }
-
-    override fun getCategories(): Flow<String> {
-        return flow {  }
-    }
+    suspend fun getCategories(): String
 }
